d157c5ca7ead2030337c0d5d1f75a1e807367e41
logangator2 <32340228+logangator2@users.noreply.github.com>
2020-07-29 13:24:49 -0500

    PlayerController Starter
    +: Added starter code for player controller

---
+++ PlaceholderProjectName/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿/*
+Written by Max Logan, July 29th 2020
+Starter Code from https://stuartspixelgames.com/2018/06/24/simple-2d-top-down-movement-unity-c/
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float horizontal, vertical;
+    Rigidbody rb;
+
+    public float runSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
