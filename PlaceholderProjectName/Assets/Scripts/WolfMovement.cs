e4853006b85f65bc64ac54408d05feef263dce1d
tscott7 <thetaylorscott@gmail.com>
2020-08-21 02:42:29 -0700

    Added NavMeshPlus package to get navmeshes working on the tilemap. Also got a rough ai working on the wolf (the code is ugly and it's giving obstacles way too wide of a berth, but it's at least following the player).

--- PlaceholderProjectName/Assets/Scripts/WolfMovement.cs
+++ PlaceholderProjectName/Assets/Scripts/WolfMovement.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 
 public class WolfMovement : MonoBehaviour
 {
     public float horizontal, vertical;
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     public Animator anim;
     public float movementSpeed = 20f;
     public int movementDir = 0;
+    public GameObject player;
+    public NavMeshAgent agent;
+    private Vector3 oldPosition, velocity;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        StartCoroutine(WaitForDirChange());
+        //StartCoroutine(WaitForDirChange());
     }
     void Awake()
     {
@@ -25,7 +30,65 @@ public class WolfMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+        //Debug.Log(this.GetComponentInChildren<NavMeshAgent>().isOnNavMesh);
+        // Moves the sprite with the agent.
+        velocity = oldPosition - transform.position;
+        oldPosition = transform.position;
+        horizontal = 0;
+        vertical = 0;
+        if (velocity.x > 0f)
+        {
+            // Moving left
+            horizontal = -1;
+        }
+        else if (velocity.x < 0f)
+        {
+            // Moving right
+            horizontal = 1;
+        }
+        if (velocity.y > 0f)
+        {
+            // Moving down
+            vertical = -1;
+        }
+        else if (velocity.y < 0f)
+        {
+            // Moving up
+            vertical = 1;
+        }
+
+        // TODO: Clean code below, it's mostly redundant because of the above.
+        if (horizontal == -1)
+        {
+            movementDir = 4;
+        }
+        else if (horizontal == 1)
+        {
+            movementDir = 2;
+        }
+        else
+        {
+            if (vertical == -1)
+            {
+                movementDir = 3;
+            }
+            else if (vertical == 1)
+            {
+                movementDir = 1;
+            }
+            else
+            {
+                movementDir = movementDir * -1;
+            }
+        }
+
+        anim.SetInteger("MovementDirection", movementDir);
+
+        transform.position = agent.transform.position;
+        agent.destination = player.transform.position;
+        //rb.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+        //localVelocity = transform.InverseTransformDirection(rb.velocity);
+        //Debug.Log(localVelocity);
     }
 
     void FixedUpdate()
@@ -33,37 +96,14 @@ public class WolfMovement : MonoBehaviour
         
     }
 
+    // TODO: Change sprite direction based on movement direction
     IEnumerator WaitForDirChange()
     {
         while(true)
         {
             horizontal = Random.Range(-1, 2);
             vertical = Random.Range(-1, 2);
-            if (horizontal == -1)
-            {
-                movementDir = 4;
-            }
-            else if (horizontal == 1)
-            {
-                movementDir = 2;
-            }
-            else
-            {
-                if (vertical == -1)
-                {
-                    movementDir = 3;
-                }
-                else if (vertical == 1)
-                {
-                    movementDir = 1;
-                }
-                else
-                {
-                    movementDir = movementDir * -1;
-                }
-            }
             
-            anim.SetInteger("MovementDirection", movementDir);
             yield return new WaitForSeconds(3);
         }
     }
